31259580fac510e2d70e0fd636e5d541fd337d93
hliaqat <hliaqat@sfu.ca>
2021-08-04 15:37:42 -0400

    added api endpoints

---
+++ backend/decisionTree.py
@@ -0,0 +1,56 @@
+import sklearn
+import numpy as np
+import pandas as pd
+import csv
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+
+import sys
+# sys.modules['sklearn.externals.six'] = six
+# from id3 import Id3Estimator
+# from id3 import export_graphviz
+from sklearn.tree import _tree
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+import pickle
+
+origCluster = pd.read_csv('data/clusterData.csv', index_col=0)
+
+# removiedNoise = origCluster.groupby('cluster').mean()
+# removiedNoise = removiedNoise.applymap(lambda x: 1 if x > 2.5 else 0)
+removiedNoise = origCluster
+# removiedNoise = removiedNoise.reset_index()
+removiedNoise = removiedNoise[removiedNoise.cluster != -1]
+# removiedNoise.reset_index()
+
+# removiedNoise.to_csv('removiedNoise.csv')
+
+x = removiedNoise.loc[:, removiedNoise.columns != 'cluster']
+x.to_csv('data/x.csv')
+# x = ave.loc[:, ave.columns != 'index']
+x = x.applymap(lambda x: 1 if x > 2.5 else 0)
+
+y = pd.DataFrame(removiedNoise['cluster'])
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42)
+# ave_X_train, ave_X_test, ave_y_train, ave_y_test = train_test_split(removiedNoise, removiedNoise, test_size=0.20, random_state=42)
+
+clf = DecisionTreeClassifier(random_state=1234)
+clf = clf.fit(X_train, y_train)
+
+text_representation = tree.export_text(clf, feature_names=x.columns.tolist(), max_depth=50)
+# print(text_representation)
+
+y_pred = clf.predict(X_test)
+print(accuracy_score(y_test, y_pred))
+
+with open("text_representation.txt", "w") as text_file:
+    text_file.write(text_representation)
+
+with open('model.pkl','wb') as f:
+	pickle.dump(clf, f)
+
+
---
+++ backend/runTree.py
@@ -0,0 +1,87 @@
+import sklearn
+import numpy as np
+import pandas as pd
+import csv
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+
+import sys
+# sys.modules['sklearn.externals.six'] = six
+# from id3 import Id3Estimator
+# from id3 import export_graphviz
+from sklearn.tree import _tree
+
+import pickle
+
+from flask import Flask
+from flask import jsonify
+
+import webbrowser
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+   return 'Hello World'
+
+@app.route('/requestRoot')
+def request_root():
+    return jsonify(
+        node=0,
+        feature=feature_name[0],
+    )
+
+@app.route('/requestLeft/<node>')
+def request_left(node):
+    childsNode = int(tree_.children_left[int(node)])
+    if tree_.feature[childsNode] != _tree.TREE_UNDEFINED:
+        return jsonify(
+            node=int(childsNode),
+            feature=feature_name[childsNode],
+            isLeaf=False,
+        )
+    return jsonify(
+            node=int(childsNode),
+            cluster=next((i for i, x in enumerate(tree_.value[childsNode].flatten().tolist()) if x)),
+            isLeaf=True,
+        )
+
+@app.route('/requestRight/<node>')
+def request_right(node):
+    childsNode = int(tree_.children_right[int(node)])
+    if tree_.feature[childsNode] != _tree.TREE_UNDEFINED:
+        return jsonify(
+            node=int(childsNode),
+            feature=feature_name[childsNode],
+            isLeaf=False,
+        )
+    return jsonify(
+            node=int(childsNode),
+            cluster=next((i for i, x in enumerate(tree_.value[childsNode].flatten().tolist()) if x)),
+            isLeaf=True,
+        )
+
+@app.route('/requestMovie/<cluster>')
+def request_movie(cluster):
+    tmdb = links[links['movieId'] == clusterRecs.iloc[int(cluster)]['top1']]
+    return 'https://www.themoviedb.org/movie/' + str(int(tmdb['tmdbId'].item()))
+
+if __name__ == '__main__':
+    with open('model.pkl', 'rb') as f:
+        clf = pickle.load(f)
+
+    feature_names = pd.read_csv('data/x.csv', index_col=0).columns.tolist()
+
+    # find movie suggestion
+    clusterRecs = pd.read_csv('data/clusterRec.csv', index_col=0)
+    links = pd.read_csv('data/links.csv')
+
+    tree_ = clf.tree_
+    feature_name = [
+        feature_names[i] if i != _tree.TREE_UNDEFINED else "undefined!"
+        for i in tree_.feature
+    ]
+
+    app.run()
